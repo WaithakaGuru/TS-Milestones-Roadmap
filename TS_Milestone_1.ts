3f57add2795d315a6bb2de3713f64eeda70d76ce
WaithakaGuru <waithakaoffices@gmail.com>
2025-05-30 15:38:25 +0300

    Add the TS_Milestone_1 project
    - function to tackle basics of TypeScript
    - from varibles to to functions

---
+++ TS_Milestone_1.ts
@@ -0,0 +1,27 @@
+// Challenge 1: Sum of Two Numbers
+function addition(num1: number, num2: number) : number{
+    return  num1 + num2;
+}
+
+// Challenge 2: Convert minutes into seconds
+function convertMinToSecs(numberOfMinutes: number) : number{
+    return numberOfMinutes * 60;
+}
+
+// Challenge 3: Find Perimeter of a Rectangle
+function findRectanglePerimeter(length: number, width: number){
+    // No negative length or width
+    if(length > 0 && width > 0) return 2*length + 2*width;
+    return "Length / Width of a shape cannot be negative";
+}
+
+/* Testing Bay - testing my code */
+console.log("Test for addition of Two numbers");
+console.log(addition(-3, 7)); // 4
+console.log(addition(45, 71)); // 116
+console.log(addition(-21, -13)); // - 34 
+
+console.log("\n  Test for Coversion of Minutes to Seconds");
+console.log(convertMinToSecs(72)); // 4320
+console.log(convertMinToSecs(12)); // 720
+console.log(convertMinToSecs(.5)); // 30
